230358c2703fdb5e3eed0f4c1e88df4746b2924a
Jon Austen <djangofan@gmail.com>
2014-04-27 15:17:40 -0700

    Initial commit.

---
+++ src/test/java/qa/tests/AssertTests1.java
@@ -0,0 +1,95 @@
+package qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.internal.annotations.Sets;
+
+import java.util.Random;
+import java.util.Set;
+
+public class AssertTests1 {
+
+    Random random = new Random();
+
+    @Test
+    public void noOrderSuccess() {
+        randomWait();
+        String[] rto1 = {"boolean", "BigInteger", "List",};
+        String[] rto2 = {"List", "BigInteger", "boolean",};
+        Assert.assertEqualsNoOrder(rto1, rto2);
+    }
+
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void noOrderFailure() {
+        randomWait();
+        String[] rto1 = {"a", "a", "b",};
+        String[] rto2 = {"a", "b", "b",};
+        Assert.assertEqualsNoOrder(rto1, rto2);
+    }
+
+    @Test
+    public void intArray_Issue4() {
+        randomWait();
+        int[] intArr00 = {1};
+        int[] intArr01 = {1};
+        Assert.assertEquals(intArr00, intArr01);
+    }
+
+    @Test
+    public void int_Integer_Arrays() {
+        randomWait();
+        int[] intArr = {1, 2};
+        Integer[] integerArr = {1, Integer.valueOf(2)};
+        Assert.assertEquals(intArr, integerArr);
+        Assert.assertEquals(integerArr, intArr);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void arraysFailures_1() {
+        randomWait();
+        int[] intArr = {1, 2};
+        long[] longArr = {1, 2};
+        Assert.assertEquals(intArr, longArr);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void arraysFailures_2() {
+        randomWait();
+        int[] intArr = {1, 2};
+        Assert.assertEquals(intArr, Long.valueOf(1));
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void arraysFailures_3() {
+        randomWait();
+        long[] longArr = {1};
+        Assert.assertEquals(Long.valueOf(1), longArr);
+    }
+
+    @Test
+    public void setsSuccess() {
+        randomWait();
+        Set<Integer> set1 = Sets.newHashSet();
+        Set<Integer> set2 = Sets.newHashSet();
+        set1.add(1);
+        set2.add(1);
+        set1.add(3);
+        set2.add(3);
+        set1.add(2);
+        set2.add(2);
+        Assert.assertEquals(set1, set2);
+        Assert.assertEquals(set2, set1);
+    }
+
+    private void randomWait() {
+        try {
+            // Sleep a random length of time from 0.5s-3s
+            Thread.sleep( random.nextInt(2500) + 500 );
+        } catch ( InterruptedException e ) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
